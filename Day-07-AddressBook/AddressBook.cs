4177ef1946279a57d9dc609b39f56f1b25f5f8c2
Ramya.U <ramyayadav2016@gmail.com>
2020-10-31 23:55:23 +0530

    [Ramya] Add . Added UC_1

---
+++ Day-07-AddressBook/AddressBook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day_07_AddressBook
+{
+    class AddressBook
+    {
+        /// <summary>
+        /// Adds the contact.
+        /// </summary>
+        public void AddContact()
+        {
+            Console.WriteLine("Enter your First Name");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter your Last Name");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            string address = Console.ReadLine();
+            Console.WriteLine("Enter your city name");
+            string city = Console.ReadLine();
+            Console.WriteLine("Enter your state");
+            string state = Console.ReadLine();
+            Console.WriteLine("Enter your zip code");
+            int zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your phone number");
+            int phoneNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your Email-Id");
+            string email = Console.ReadLine();
+
+            ///Creates a reference of Contact class
+            Contact contact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+            Console.WriteLine(contact);
+        }
+    }
+}
